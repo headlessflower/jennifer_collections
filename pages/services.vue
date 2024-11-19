efa1ee279cd08b020dae4a46cb3becfefb26ed67
Gerardo Garcia <gerardojgarcia@pm.me>
2024-11-18 19:18:43 -0800

    updated services section

--- pages/services.vue
+++ pages/services.vue
@@ -3,6 +3,78 @@
 </script>
 <template>
   <div>
+    <h1>Collections Care Services</h1>
+    <div>
 
+
+      <NuxtImg src="/archives-1.webp"></NuxtImg>
+      <div>
+        <strong>Exhibition Logistics</strong>
+        <ul>
+          Want to ensure a structured install and deinstall? I have expertise in:
+          <li>Arranging shipping & insurance coverage for domestic and international shipments.</li>
+          <li>Packing objects for transportation and arranging crating.</li>
+          <li>Detailed condition reporting</li>
+          <li>Art handling for installation and deinstallation</li>
+          <li>Processing documentation to ensure your institutional history is accurate</li>
+        </ul>
+      </div>
+      <div>
+        <strong>Registration</strong>
+        <ul>
+          Want to ensure a structured install and deinstall? I have expertise in:
+          <li>Arranging shipping & insurance coverage for domestic and international shipments.</li>
+          <li>Packing objects for transportation and arranging crating.</li>
+          <li>Detailed condition reporting</li>
+          <li>Art handling for installation and deinstallation</li>
+          <li>Processing documentation to ensure your institutional history is accurate</li>
+        </ul>
+      </div>
+      <div>
+        <strong>Database Management</strong>
+        <ul>
+          Want to ensure a structured install and deinstall? I have expertise in:
+          <li>Arranging shipping & insurance coverage for domestic and international shipments.</li>
+          <li>Packing objects for transportation and arranging crating.</li>
+          <li>Detailed condition reporting</li>
+          <li>Art handling for installation and deinstallation</li>
+          <li>Processing documentation to ensure your institutional history is accurate</li>
+        </ul>
+      </div>
+      <div>
+        <strong>Collections Inventory</strong>
+        <ul>
+          Want to ensure a structured install and deinstall? I have expertise in:
+          <li>Arranging shipping & insurance coverage for domestic and international shipments.</li>
+          <li>Packing objects for transportation and arranging crating.</li>
+          <li>Detailed condition reporting</li>
+          <li>Art handling for installation and deinstallation</li>
+          <li>Processing documentation to ensure your institutional history is accurate</li>
+        </ul>
+      </div>
+      <div>
+        <strong>Archival Storage Solutions</strong>
+        <ul>
+          Want to ensure a structured install and deinstall? I have expertise in:
+          <li>Arranging shipping & insurance coverage for domestic and international shipments.</li>
+          <li>Packing objects for transportation and arranging crating.</li>
+          <li>Detailed condition reporting</li>
+          <li>Art handling for installation and deinstallation</li>
+          <li>Processing documentation to ensure your institutional history is accurate</li>
+        </ul>
+      </div>
+      <div>
+        <strong>Operations</strong>
+        <ul>
+          Want to ensure a structured install and deinstall? I have expertise in:
+          <li>Arranging shipping & insurance coverage for domestic and international shipments.</li>
+          <li>Packing objects for transportation and arranging crating.</li>
+          <li>Detailed condition reporting</li>
+          <li>Art handling for installation and deinstallation</li>
+          <li>Processing documentation to ensure your institutional history is accurate</li>
+        </ul>
+      </div>
+
+    </div>
   </div>
 </template>
